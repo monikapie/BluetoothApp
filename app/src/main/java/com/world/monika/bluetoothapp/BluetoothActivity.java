628df427c0beea7914d78943d2a5403e31dda2f0
pietyszukm <monika.pietyszuk@gmail.com>
2016-07-05 16:18:21 +0200

    Please develop Android application capable of: * Store each unique device's data in history, use another table/list * Stored data should persist * Implement devices pairing (if possible) * Use list adapter
    EDIT - new requirement
    Application should be able to log signal information to text file after pressing button. The same button should stop logging process. File format:
    [SSID] [MAC] [SIGNAL STRENGTH %] [RSSI]
    
    Zrobione:
    
    * Scanning for available bluetooth devices +
    * Displaying all found devices as a list +
    http://stackoverflow.com/questions/10795424/how-to-get-the-bluetooth-devices-as-a-list
    * Device specific data should be displayed after selecting any record + ( wyświetlane za pomocą Toastów)
    
    Dodatkowe:
    https://github.com/apache/pig/blob/89c2e8e76c68d0d0abe6a36b4e08ddc56979796f/src/org/apache/pig/impl/util/ObjectSerializer.java
    przechowywanie tablicy w sharedpreferances
    
    Powyższy kod ograniczony jest licencja
    Użyje setów

--- app/src/main/java/com/world/monika/bluetoothapp/BluetoothActivity.java
+++ app/src/main/java/com/world/monika/bluetoothapp/BluetoothActivity.java
@@ -8,30 +8,42 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.ArraySet;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 public class BluetoothActivity extends AppCompatActivity {
     Button b1;
     Button b2;
+    Button b3;
     TextView tv1;
     ListView lv1;
     private ArrayList<String> deviceList = new ArrayList<String>();
+    private ArrayList<String> deviceListPlus = new ArrayList<String>();
+    private ArrayList<String> deviceListAll = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bluetooth);
+
         b1=(Button)findViewById(R.id.button1);
         b2=(Button)findViewById(R.id.button2);
+        b3=(Button)findViewById(R.id.button3);
         tv1=(TextView)findViewById(R.id.textview1);
         lv1=(ListView)findViewById(R.id.listview1);
 
@@ -66,23 +78,50 @@ public class BluetoothActivity extends AppCompatActivity {
     }
     private final BroadcastReceiver reciever = new BroadcastReceiver() {
         @Override
-        public void onReceive(Context context, Intent intent) {
+        public void onReceive(Context context, final Intent intent) {
             String action = intent.getAction();
             if(BluetoothDevice.ACTION_FOUND.equals(action)){
                 BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                String mac = device.getAddress();
+                String name = device.getName();
+                int rssi = intent.getShortExtra(BluetoothDevice.EXTRA_RSSI,Short.MIN_VALUE);
                 String status="";
                 if(device.getBondState() != BluetoothDevice.BOND_BONDED) {
                     status = "nie sparowane";
                 }else{
                     status="sparowane";
                 }
-                deviceList.add(device.getName()+"\n"+device.getAddress());
-                Log.d("INFO", "Znaleziono urządzenie: "+device.getName()+" - "+device.getAddress()+" - "+status);
-                lv1.setAdapter(new ArrayAdapter<String>(context, android.R.layout.simple_list_item_1, deviceList));
+                deviceList.add(name);
+                deviceListPlus.add(mac+"\n"+(rssi+100)*2+"%"+"\n"+rssi+"dB");
+                deviceListAll.add(name+"\n"+mac+"\n"+(rssi+100)*2+"%"+"\n"+rssi+"dB");
+                //System.out.println(Arrays.toString(deviceListAll.toArray()));
             }
         }
-    };
 
+    };
+    @Override
+    public void onPause(){
+        unregisterReceiver(reciever);
+        super.onPause();
+        int i = 0;
+        for(i=0; i < deviceList.size();i++)
+            Log.d("INFO", "Znaleziono urządzenie: "+deviceList.get(i));
+        lv1.setAdapter(new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1, deviceList));
+        lv1.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                showToast(deviceListPlus.get(i));
+            }
+        });
+        b3.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                Set<String> deviceSet = new HashSet<String>();
+                deviceSet.addAll(deviceListPlus);
+                saveData(deviceSet);
+                showToast("Data saved");
+            }
+        });
+    }
     public void beVisible(){
         Log.d("INFO", "I am visible");
         Intent beVisible = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
@@ -96,6 +135,17 @@ public class BluetoothActivity extends AppCompatActivity {
         this.registerReceiver(reciever, filter);
         BluetoothAdapter ba = BluetoothAdapter.getDefaultAdapter();
         ba.startDiscovery();
+    }
+
+    public void showToast(String text){
+        Toast.makeText(getApplicationContext(),text,Toast.LENGTH_SHORT).show();
+    }
+
+    public void saveData(Set details){
+        SharedPreferences bluetoothPref = getApplicationContext().getSharedPreferences("BluePref", MODE_PRIVATE );
+        SharedPreferences.Editor editor = bluetoothPref.edit();
+        editor.putStringSet("key",details);
+        editor.commit();
     }
 
 }
