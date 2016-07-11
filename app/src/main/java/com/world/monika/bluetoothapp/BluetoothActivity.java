e8b70de9af4a51801c151d8ae09ef98ff61dc543
pietyszukm <monika.pietyszuk@gmail.com>
2016-07-11 16:19:10 +0200

    Wszystkie założenia aplikacji spełnione, jedyne co można naprawić to "pierwsze skanowanie"

--- app/src/main/java/com/world/monika/bluetoothapp/BluetoothActivity.java
+++ app/src/main/java/com/world/monika/bluetoothapp/BluetoothActivity.java
@@ -1,23 +1,19 @@
 package com.world.monika.bluetoothapp;
 
-import android.app.Activity;
+import android.annotation.TargetApi;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
-import android.content.BroadcastReceiver;
+import android.bluetooth.BluetoothManager;
 import android.content.Context;
 import android.content.Intent;
-import android.content.IntentFilter;
-import android.content.SharedPreferences;;
+import android.content.pm.PackageManager;
 import android.media.MediaScannerConnection;
-import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.ArraySet;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -29,208 +25,105 @@ import java.io.IOException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Calendar;
-import java.util.Date;
-import java.util.HashSet;
-import java.util.Set;
 
+@TargetApi(18)
 public class BluetoothActivity extends AppCompatActivity {
-    Button b1;
-    Button b2;
-    Button b3;
-    Button b4;
-    Button b5;
+    int summand = 0;
     TextView tv1;
     TextView tv2;
+    TextView tv4;
     TextView row;
     ListView lv1;
     private ArrayList<String> deviceList = new ArrayList<String>();
     private ArrayList<String> deviceListPlus = new ArrayList<String>();
     private ArrayList<String> deviceListAll = new ArrayList<String>();
+    private ArrayList<String> macList = new ArrayList<String>();
     public final static String tag = "INFO";
-    private static final long SCAN_PERIOD = 10000;
+    private BluetoothAdapter mBluetoothAdapter;
+
+    private static final int REQUEST_ENABLE_BT = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bluetooth);
-
-        b1 = (Button) findViewById(R.id.button1);
-        b2 = (Button) findViewById(R.id.button2);
-        b3 = (Button) findViewById(R.id.button3);
-        b4 = (Button) findViewById(R.id.button4);
-        b5 = (Button) findViewById(R.id.button5);
         tv1 = (TextView) findViewById(R.id.textview1);
         tv2 = (TextView) findViewById(R.id.textview2);
+        tv4 = (TextView) findViewById(R.id.textview4);
         row = (TextView) findViewById(R.id.Row);
         lv1 = (ListView) findViewById(R.id.listview1);
 
-        Log.e(tag, "Program is started.");
-        //Scanning
-        b1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                scanVisibleDevices();
-            }
-        });
-        //Changing visiability
-        b2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                becomeVisible();
-            }
-        });
-        b5.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-//                showHistory();
-            }
-        });
-        //Checking local MAC
-        BluetoothAdapter ba = BluetoothAdapter.getDefaultAdapter();
-        tv1.setText("Your mac:" + ba.getAddress());
-        Log.e(tag, "Your device address:" + ba.getAddress());
-        //Starting intent
-        if (!ba.isEnabled()) {
-            Intent intent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-            startActivityForResult(intent, 1);
+
+        tv4.setText("Visible devices with BLE");
+
+        // Checks whether BLE is supported on the device.
+        if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)) {
+            Toast.makeText(this, R.string.ble_not_supported, Toast.LENGTH_SHORT).show();
+            finish();
         }
+
+        // Initializes a Bluetooth adapter.(API level 18 and above, get a reference to
+        // BluetoothAdapter through BluetoothManager.)
+        final BluetoothManager bluetoothManager =
+                (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
+        mBluetoothAdapter = bluetoothManager.getAdapter();
+
+        // Checks whether Bluetooth is supported on the device.
+        if (mBluetoothAdapter == null) {
+            Toast.makeText(this, R.string.error_bluetooth_not_supported, Toast.LENGTH_SHORT).show();
+            finish();
+            return;
+        }
+
+        Log.i(tag, "Program is started.");
     }
 
-    //????
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
-        if (resultCode == Activity.RESULT_OK) {
-            Log.e(tag, "Accepted!");
-            //BluetoothAdapter ba = BluetoothAdapter.getDefaultAdapter();
+    protected void onResume() {
+        super.onResume();
+        // If Bluetooth is not currently enabled, ask if BLE can be enabled.
+        if (!mBluetoothAdapter.isEnabled()) {
+            if (!mBluetoothAdapter.isEnabled()) {
+                Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+            }
         }
+        scanLeDevice(true);
     }
 
-    //BroadcastReciever, control over bluetooth
-    private final BroadcastReceiver reciever = new BroadcastReceiver() {
-        @Override
-        public void onReceive(Context context, final Intent intent) {
-            String action = intent.getAction();
-            //What happend when bluetooth is starting
-            if (BluetoothAdapter.ACTION_DISCOVERY_STARTED.equals(action)) {
-            }
-            //What happend when bluetooth action finished
-            else if (BluetoothAdapter.ACTION_DISCOVERY_FINISHED.equals(action)) {
-                tv2.setText("");
-                //List adapter - CHANGE INTO MAP!!
-                lv1.setAdapter(new ArrayAdapter<String>(BluetoothActivity.this, R.layout.row, deviceList));
-                //Listing details about devices after clicking on row
-                lv1.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-                    @Override
-                    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                        showToast(deviceListPlus.get(i));
-                    }
-                });
-                //Saving data to Shared Preferances - REPAIR
-                b3.setOnClickListener(new View.OnClickListener() {
-                    public void onClick(View view) {
-                        Set<String> deviceSet = new HashSet<String>();
-                        deviceSet.addAll(deviceListPlus);
-                        saveData(deviceSet);
-                        showToast("Data saved");
-                    }
-                });
-                //Saving data to File (External)
-                b4.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        saveToFile(deviceListAll);
-                    }
-                });
-                //Saving visible devices in lists
-            } else if (BluetoothDevice.ACTION_FOUND.equals(action)) {
-                tv2.setText("Scanning...");
-                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
-                String mac = device.getAddress();
-                String name = device.getName();
-                int rssi = intent.getShortExtra(BluetoothDevice.EXTRA_RSSI, Short.MIN_VALUE);
-                Log.e(tag, "Found device: " + mac + name);
-                deviceList.add(name);
-                deviceListPlus.add(mac + "\n" + (rssi + 100) * 2 + "%" + "\n" + rssi + "dB");
-                deviceListAll.add(name + "\n" + mac + "\n" + (rssi + 100) * 2 + "%" + "\n" + rssi + "dB");
-            }
-        }
-
-    };
-
-    //Unregistering reciever
     @Override
-    public void onPause() {
-        unregisterReceiver(reciever);
+    protected void onPause() {
         super.onPause();
+        scanLeDevice(false);
+
     }
 
-    //Changing visiability
-    public void becomeVisible() {
-        Log.e(tag, "I am visible");
-        Intent beVisible = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
-        beVisible.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 300);
-        startActivity(beVisible);
-    }
-
-    //Scanning
-    public void scanVisibleDevices() {
-        deviceList.clear();
-        deviceListPlus.clear();
-        deviceListAll.clear();
-        Log.e(tag, "I am looking for visible devices");
-        IntentFilter filter = new IntentFilter();
-        filter.addAction(BluetoothDevice.ACTION_FOUND);
-        filter.addAction(BluetoothAdapter.ACTION_DISCOVERY_STARTED);
-        filter.addAction(BluetoothAdapter.ACTION_DISCOVERY_FINISHED);
-        this.registerReceiver(reciever, filter);
-        BluetoothAdapter ba = BluetoothAdapter.getDefaultAdapter();
-        ba.startDiscovery();
-    }
-
-    //Showing toast
     public void showToast(String text) {
         Toast.makeText(getApplicationContext(), text, Toast.LENGTH_SHORT).show();
     }
 
-    //Saving data in Shared Preferences
-    public void saveData(Set details) {
-        Log.e(tag, "Saving data to SP");
-        SharedPreferences bluetoothPref = getApplicationContext().getSharedPreferences("BluePref", MODE_PRIVATE);
-        SharedPreferences.Editor editor = bluetoothPref.edit();
-        String k = "dg";
-        editor.putStringSet(k, details);
-        editor.commit();
-    }
-
     //Saving to file
     public void saveToFile(ArrayList listOfDevices) {
-        Log.e(tag, "Saving data to file");
-        String SLOD = "";
-        DateFormat df = new SimpleDateFormat("d MMM yyyy, HH:mm");
-        String date = df.format(Calendar.getInstance().getTime());
+        Log.i(tag, "Saving data to file");
+        String SListOfDevices = "";
+        DateFormat dataFormat = new SimpleDateFormat("d MMM yyyy, HH:mm");
+        String date = dataFormat.format(Calendar.getInstance().getTime());
         try {
-            // Creates a trace file in the primary external storage space of the
-            // current application.
-            // If the file does not exists, it is created.
+            // Saving to primary external storage space of the current application.
             File traceFile = new File(((Context) this).getExternalFilesDir(null), "TraceFile.txt");
-            if (!traceFile.exists())
+            if (!traceFile.exists()){
                 traceFile.createNewFile();
-            // Adds a line to the trace file
-            BufferedWriter writer = new BufferedWriter(new FileWriter(traceFile, true /*append*/));
-            writer.write(date);
-            writer.write("---------------------------------------------------------------------\n");
+            }
+            BufferedWriter writer = new BufferedWriter(new FileWriter(traceFile, true));
+            writer.write("\n"+date);
+            writer.write("  ");
             for (int i = 0; i < listOfDevices.size() - 1; i++) {
-                SLOD = (String) listOfDevices.get(i);
-                writer.write(SLOD);
-                writer.write("\n");
+                SListOfDevices = (String) listOfDevices.get(i);
+                writer.write(SListOfDevices);
             }
             writer.close();
-            // Refresh the data so it can seen when the device is plugged in a
-            // computer. You may have to unplug and replug the device to see the
-            // latest changes. This is not necessary if the user should not modify
-            // the files.
+            // Refresh the data.
             MediaScannerConnection.scanFile((Context) (this),
                     new String[]{traceFile.toString()},
                     null,
@@ -240,13 +133,111 @@ public class BluetoothActivity extends AppCompatActivity {
         }
     }
 
-//    public void showHistory() {
-//        try {
-//
-//        } catch (Exception e) {
-//            Log.e(tag, "File with this name doesn't exist.");
-//            showToast("File with this name doesn't exist.");
-//        }
-//
-//    }
+    Thread timer = new Thread() {
+        public void run () {
+            for (;;) {
+                try {
+                    Thread.sleep(10000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                saveToFile(deviceListAll);
+            }
+        }
+    };
+
+    private void scanLeDevice(final boolean enable) {
+        if (enable) {
+            deviceList.clear();
+            deviceListAll.clear();
+            macList.clear();
+            macList.add(" ");
+            timer.start();
+            mBluetoothAdapter.startLeScan(mLeScanCallback);
+        } else {
+            mBluetoothAdapter.stopLeScan(mLeScanCallback);
+            Log.i(tag, "Scanning stopped and details saved to file");
+        }
+    }
+
+    private String getUuid(byte[] scanRecord) {
+        int startByte = 2;
+        boolean patternFound = false;
+        String uuid="";
+        while (startByte <= 5) {
+            if (((int) scanRecord[startByte + 2] & 0xff) == 0x02 && //Identifies an iBeacon
+                    ((int) scanRecord[startByte + 3] & 0xff) == 0x15) { //Identifies correct data length
+                patternFound = true;
+                break;
+            }
+            startByte++;
+        }
+
+        if (patternFound) {
+            //Convert to hex String
+            byte[] uuidBytes = new byte[16];
+            System.arraycopy(scanRecord, startByte + 4, uuidBytes, 0, 16);
+            String hexString = bytesToHex(uuidBytes);
+
+            //Here is your UUID
+            uuid = hexString.substring(0, 8) + "-" +
+                    hexString.substring(8, 12) + "-" +
+                    hexString.substring(12, 16) + "-" +
+                    hexString.substring(16, 20) + "-" +
+                    hexString.substring(20, 32);
+
+            //Here is your Major value
+            int major = (scanRecord[startByte + 20] & 0xff) * 0x100 + (scanRecord[startByte + 21] & 0xff);
+
+            //Here is your Minor value
+            int minor = (scanRecord[startByte + 22] & 0xff) * 0x100 + (scanRecord[startByte + 23] & 0xff);
+        }
+        return uuid;
+    }
+
+    static final char[] hexArray = "0123456789ABCDEF".toCharArray();
+    private static String bytesToHex(byte[] bytes) {
+        char[] hexChars = new char[bytes.length * 2];
+        for ( int j = 0; j < bytes.length; j++ ) {
+            int v = bytes[j] & 0xFF;
+            hexChars[j * 2] = hexArray[v >>> 4];
+            hexChars[j * 2 + 1] = hexArray[v & 0x0F];
+        }
+        return new String(hexChars);
+    }
+
+    private BluetoothAdapter.LeScanCallback mLeScanCallback =
+            new BluetoothAdapter.LeScanCallback() {
+
+                @Override
+                public void onLeScan(final BluetoothDevice device, final int rssi, final byte[] scanRecord) {
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            //Log.i(tag,"Scanning..");
+                            String mac = device.getAddress();
+                            String name = device.getName();
+                            String uuid = getUuid(scanRecord);
+                            int strength =  (rssi+100)*2;
+
+                            for(int i = 0; i <= macList.size(); i++){
+                                if(!macList.contains(mac)){
+                                    macList.add(mac);
+                                    deviceList.add(name+" "+mac);
+                                    deviceListAll.add(name+" "+mac+" "+rssi+" "+strength+" "+uuid);
+                                    deviceListPlus.add(rssi+" "+strength+" "+uuid);
+                                }
+                            }
+                            lv1.setAdapter(new ArrayAdapter<String>(BluetoothActivity.this, R.layout.row, deviceList));
+                            //Listing details about devices after clicking on row
+                            lv1.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                                @Override
+                                public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                                    showToast(deviceListPlus.get(i));
+                                }
+                            });
+                        }
+                    });
+                }
+            };
 }
